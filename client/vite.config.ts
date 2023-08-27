d9a007eb66d7e6f7a4103f6f0cf39a1c0110cd1c
Tanmay Banage <banagetanmay@gmail.com>
2023-08-27 19:02:30 +0530

    trying something

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,25 +1,15 @@
-import { resolve } from "path";
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
-  plugins: [react()],
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: "src/index.html",
-        home: "src/pages/home/index.html",
-        greet: "src/greet/index.html",
-        login: "src/pages/login/index.html",
-        register: "src/pages/Register.tsx",
-      },
+  base: "/",
+  history: "hash",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [react()],
 });
