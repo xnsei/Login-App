06fa365db3d4bf03718def91bf9662675b7e816f
Tanmay Banage <banagetanmay@gmail.com>
2023-08-27 19:08:46 +0530

    adding vercel.json for clientSide

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  history: "hash",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
